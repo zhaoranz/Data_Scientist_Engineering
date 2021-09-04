29366a3b1a2a93e1015135ea7c43cd150ea2f053
zhaoranz <zhaoran1124@gmail.com>
2021-09-04 12:41:17 +0200

    Update 155. Min Stack.py

--- Algorithm_Python/Ch1_stack_and_queue/155. Min Stack.py
+++ Algorithm_Python/Ch1_stack_and_queue/155. Min Stack.py
@@ -1,3 +1,10 @@
+'''
+假设当前数据为newNum，先将其压入stackData。然后判断stackMin 是否为空。
+如果为空，则newNum 也压入stackMin；#math.inf# 如果不为空，则比较newNum 和stackMin 的栈顶
+元素中哪一个更小。
+如果newNum 更小或两者相等，则newNum 也压入stackMin；如果stackMin 中栈顶元素
+小，则把stackMin 的栈顶元素重复压入stackMin，即在栈顶元素上再压入一个栈顶元素
+'''
 class MinStack:
     def __init__(self):
         self.stack = []
