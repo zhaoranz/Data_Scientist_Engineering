ab2f11cad5df6edb149c884daac658edc4aebc38
zhaoranz <zhaoran1124@gmail.com>
2021-09-27 14:15:07 +0200

    Update 176. Second Highest Salary.sql

--- SQL/176. Second Highest Salary.sql
+++ SQL/176. Second Highest Salary.sql
@@ -8,6 +8,13 @@ limit 1 offset 1), NULL
 )
 as SecondHighestSalary
 
+# Write your MySQL query statement below
+select max(Salary) SecondHighestSalary 
+from Employee
+where Salary < (select max(Salary) from Employee)
+);
+
+
 #mssql
 /* Write your T-SQL query statement below */
 select ISNULL((select distinct Salary from Employee
