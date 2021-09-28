802d89f5fc6da94f7599bd9720aca573a934b84e
zhaoranz <zhaoran1124@gmail.com>
2021-09-28 16:11:42 +0200

    Update 178. Rank Scores.sql

--- SQL/178. Rank Scores.sql
+++ SQL/178. Rank Scores.sql
@@ -1,4 +1,11 @@
+## Write your MySQL query statement below
+select a.Score as Score,
+(select count(distinct b.Score) from Scores b where b.Score>=a.Score) as `Rank`
+from Scores a
+order by a.Score desc
+
 # Write your MySQL query statement below
+##Faster, more efficient
 
 select t.score Score,t.rank `Rank` from scores join
 (
